a066ed81bc7335d199d78d5510246ebb2c9d19af
charlesBarboleda <166056415+charlesBarboleda@users.noreply.github.com>
2026-02-10 14:18:13 -0800

    Added new package: NaughtyAttributes. Finished parameter:  Time Digits.
    New package: NaughtyAttributes expands Unity's range of attributes for easier development. Created new script: TimeDigitsManager.cs which handles all 'TIME_DIGITS' parameter logic.

---
+++ Assets/Scripts/UI/ParametersManagers/TimeDigitsManager.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+
+public class TimeDigitsManager : MonoBehaviour, IAPIParameterManager, IDefaultable
+{
+    [SerializeField] TMP_Dropdown _timeDigitsDropdown;
+
+    public bool TryGetURL(out string URL)
+    {
+        TimeDigitsDropdown selection = (TimeDigitsDropdown)_timeDigitsDropdown.value;
+        string value = selection.ToString().ToUpperInvariant();
+        URL = $"TIME_DIGITS={HorizonsAPIParameters.EncodeQuoted(value)}";
+        return true;
+    }
+
+    public void ApplyDefault()
+    {
+        _timeDigitsDropdown.value = 0;
+        _timeDigitsDropdown.RefreshShownValue();
+    }
+
+    enum TimeDigitsDropdown
+    {
+        Minutes,
+        Seconds,
+        FracSec
+    }
+}
